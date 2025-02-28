06a0ccead4ae6ffe19cc043e9ad312d95c18f2cd
Umar Hassan <umar@finehomeint.com>
2025-02-28 11:32:31 +0400

    Commit Till Test Case 7(Bikes Sorting and Selection

--- src/test/java/Site_Automation.java
+++ src/test/java/Site_Automation.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 import java.util.List;
@@ -20,6 +22,8 @@ public class Site_Automation {
     WebDriver driver = new ChromeDriver();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     Actions act = new Actions(driver);
+    SoftAssert softassert = new SoftAssert();
+
 
 
     @Test(priority = 1)
@@ -56,7 +60,7 @@ public class Site_Automation {
         String currentWindow = driver.getWindowHandle();
         System.out.println("Current Window handle: " + currentWindow);
         Set<String> allwindowHandles = driver.getWindowHandles();
-        System.out.println(allwindowHandles);
+        System.out.println("All Window Handles till now: " + allwindowHandles);
 
         for(String window:allwindowHandles){
             if(!window.equals(currentWindow)){
@@ -92,14 +96,14 @@ public class Site_Automation {
 
     @Test(priority = 5)
     public void InspectionReport() throws InterruptedException {
-        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,200)");
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,700)");
         Thread.sleep(2000);
         WebElement viewSample = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("View Sample Inspection Report")));
         viewSample.click();
         String currentWindow2 = driver.getWindowHandle();
         System.out.println("Current Window handle: " + currentWindow2);
         Set<String> allhandlesupdated = driver.getWindowHandles();
-        System.out.println("All Window handles: " + allhandlesupdated );
+        System.out.println("All Window Handles till now: " + allhandlesupdated );
 
         for(String i:allhandlesupdated){
              if(!i.equals(currentWindow2)){
@@ -114,12 +118,54 @@ public class Site_Automation {
     public void searchBikes() throws InterruptedException {
         WebElement navigatetoBikes = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[2]/ul/li[3]/a")));
         navigatetoBikes.click();
-        WebElement usedBikes = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[2]/ul/li[3]/ul/li/div[1]/ul/li[1]/a")));
-        usedBikes.click();
-        System.out.println(driver.getCurrentUrl());
-        //Assert.assertEquals(driver.findElement(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[3]/div[2]/button")).getText(), "Search");
-        //driver.findElement(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[3]/div[2]/button")).click();
-        //BikeMake.sendKeys("Ybr");
+        System.out.println("Current URL: " + driver.getCurrentUrl());
+        //WebElement usedBikes = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Find Used Bikes")));
+        //usedBikes.click();
+        String x = "More Search Option";
+        String buttonText = "Searc";
+
+        //Assertion for Search Options
+        softassert.assertEquals(driver.findElement(By.id("more_option")).getText(), x);
+
+        //Assertion for Button Text
+        softassert.assertEquals(driver.findElement(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[3]/div[2]/button")).getText(), buttonText);;
+
+        WebElement BikeMake = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[1]/div/ul/li[1]/input[1]")));
+        BikeMake.sendKeys("YBR");
+
+        WebElement citySelection = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[1]/div/ul/li[2]/div/a")));
+        citySelection.click();
+
+        driver.findElement(By.id("UsedCity_chzn_o_4")).click();
+
+        WebElement priceRange = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[1]/div/ul/li[3]/div")));
+        priceRange.click();
+        driver.findElement(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[1]/div/ul/li[3]/div/div/div/div/div[1]/span/input")).sendKeys("100");
+        driver.findElement(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[1]/div/ul/li[3]/div/div/div/div/div[2]/span/input")).sendKeys("300");
+        driver.findElement(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[1]/div/ul/li[3]/div/i")).click();
+        WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[5]/section[2]/div[2]/div[2]/div[1]/div[3]/div[2]/button")));
+        searchButton.click();
+
+    }
+
+    @Test(priority = 7)
+    public void sortandSelectBikes(){
+
+        WebElement sortBy = wait.until(ExpectedConditions.elementToBeClickable(By.id("sortby")));
+        Select selectSort = new Select(sortBy);
+        selectSort.selectByVisibleText("Price: High to Low");
+        softassert.assertTrue(sortBy.isEnabled());
+        WebElement gridView = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[6]/section[2]/div/div/div[3]/div/div[2]/div[1]/div/div[2]/div/button[2]")));
+        gridView.click();
+    }
+
+
+    @AfterClass
+    public void afterClassOperations() throws InterruptedException {
+        softassert.assertAll("[]");
+        //Thread.sleep(3000);
+        driver.close();
+
     }
 }
   
